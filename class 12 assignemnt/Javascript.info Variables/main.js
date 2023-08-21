4002167fc9f8cc171d63d23d25d233206da42270
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-08-21 21:19:22 +0300

    Add files via upload

---
+++ class 12 assignemnt/Javascript.info Variables/main.js
@@ -0,0 +1,24 @@
+//Task 1
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+//Task 2
+//Giving the right name
+// Create a variable with the name of our planet. How would you name such a variable?
+let ourPlanetName;
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let visitorName;
+
+//Task 3
+//Uppercase const?
+const birthday = '18.04.1982'; //We can make it uppercase
+
+const age = someCode(birthday); //We should not make it uppercase
