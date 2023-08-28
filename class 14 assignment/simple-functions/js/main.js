347ab220e058f9d4aff17fbe210a6e9ecfd1e6a5
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-08-28 16:43:52 +0300

    Add files via upload

---
+++ class 14 assignment/simple-functions/js/main.js
@@ -0,0 +1,25 @@
+//create a function that adds two numbers and alerts the sum
+function add(num1, num2) {
+
+    alert(num1 + num2)
+}
+
+add(2, 4);
+
+//create a function that multiplys three numbers and console logs the product
+function mult(num1, num2, num3) {
+
+    console.log(num1 * num2 * num3);
+}
+
+mult(4, 3, 2);
+
+//create a function that divides two numbers and returns the ???
+function divides(num1, num2) {
+
+    return num1 / num2
+}
+
+divides(4, 2);
+divides(10, 2);
+divides(6, 3);
