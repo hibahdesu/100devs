e8162925fa0a5c5ae6d442fa822d21ff603471a3
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-08-30 19:47:23 +0300

    Add files via upload

---
+++ class 14 assignment/practice/js/script.js
@@ -0,0 +1,40 @@
+let brief = document.querySelector('.brief');
+let music = document.querySelector('.music');
+let photos = document.querySelector('.photos');
+let characters = document.querySelector('.characters');
+let series = document.querySelector('.series');
+
+let briefBtn = document.querySelector('#brief');
+let musicBtn = document.querySelector('#music');
+let photosBtn = document.querySelector('#photos');
+let characterBtn = document.querySelector('#characters');
+let seriesBtn = document.querySelector('#series');
+
+briefBtn.addEventListener('click', showBrief)
+musicBtn.addEventListener('click', showMusic);  //showing the list of all movies in a genre
+photosBtn.addEventListener('click', showPhotos);//showing the list of all pictures in a genre
+characterBtn.addEventListener('click', showCharacters)//showing the list of all people in a movie or tv-serie
+seriesBtn.addEventListener('click', showSeries);
+
+function showBrief() {
+
+}
+
+
+function showMusic() {
+
+}
+
+function showPhotos() {
+
+}
+
+function showCharacters() {
+
+}
+
+function showSeries() {
+    
+}
+
+
