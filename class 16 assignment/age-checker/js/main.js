18c62d38661114b97590a21f09270c1b39175d7d
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-09-01 21:03:56 +0300

    Add files via upload

---
+++ class 16 assignment/age-checker/js/main.js
@@ -0,0 +1,40 @@
+//Create a conditonal that checks their age
+//If under 16, tell them they can not drive
+//If under 18, tell them they can't hate from outside the club, because they can't even get in
+//If under 21, tell them they can not drink
+//If under 25, tell them they can not rent cars affordably
+//If under 30, tell them they can not rent fancy cars affordably
+//If under over 30, tell them there is nothing left to look forward too
+let age = 19;
+
+if (age < 16) {
+    console.log('You they can not drive');
+}
+else if (age < 18) {
+    console.log("You can't hate from out side of the club");
+}
+else if (age < 21) {
+    console.log ('you cannot drink') ;   
+}
+else if (age < 25) {
+    console.log ("You can't rent car affordable")
+} 
+else if (age < 30) {
+    console.log ("You can't rent fancy car affordable")
+}
+else if (age >= 30) {
+    console.log( "Nothing left for you to look forward too" )   
+}
+
+//--- Harder
+//On click of the h1
+let h1 = document.querySelector('h1');
+h1.addEventListener('click', check);
+//Take the value from the input
+function check() {
+    let input = document.querySelector('input').value;
+    let p = document.querySelector('p')
+    p.innerText = input;
+}
+
+//Place the result of the conditional in the paragraph
