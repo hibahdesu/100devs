6302cf1d25c70de92cce749bff1a070ec6f3602a
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-09-02 16:10:39 +0300

    Update main.js

--- class 16 assignment/stop-snacking/js/main.js
+++ class 16 assignment/stop-snacking/js/main.js
@@ -1,1 +1,19 @@
-//Create a function that grabs the number of snacks from the input and tells you to stop that many times
+//Create a function that grabs the number of snacks 
+// from the input and tells you to stop that many times
+
+let help = document.querySelector('#help');
+help.addEventListener('click', snacks)
+
+function snacks() {
+    let input = document.querySelector('input').value;
+
+    let stops = document.querySelector('#stops');
+
+    for (let i = 1; i <= Number(input); i++) {
+
+        console.log("Stop");
+        
+        stops.innerText += ' Stop Snacking'
+    }
+}
+
