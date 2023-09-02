73ea682779606a03334e15d709e47f0817796edb
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-09-02 16:00:03 +0300

    Update main.js

--- class 16 assignment/sir-savage-the-21st/js/main.js
+++ class 16 assignment/sir-savage-the-21st/js/main.js
@@ -1,17 +1,20 @@
 //Create a function that has a loop that prints '21' 
 // 21 times to the console and then call that function
 
-function looping() {
-    console.log('21');
+function looping21() {
+
+    for (let i = 1; i < 22; i++) {
+            console.log('21');
+            
+            //Bonus can you make it print '21' 21 times to the dom?
+            let savageSays = document.querySelector('#savageSays');
+            savageSays.innerText += ' 21';
+    
+    }
+    
 }
 
-for (let i = 1; i < 22; i++) {
-    console.log(i);
-    looping();
-    let savageSays = document.querySelector('#savageSays');
-    savageSays.innerText = i;
+looping21();
 
-}
-//Bonus can you make it print '21' 21 times to the dom?
 
 
