a95d4060a6ffd45239a9e7edff39b3bbe74f77d9
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2023-09-10 17:46:22 +0300

    Update main.js

--- class 19 assignment/bring-it-on-all-or-nothing/js/main.js
+++ class 19 assignment/bring-it-on-all-or-nothing/js/main.js
@@ -38,14 +38,14 @@ takeBool(true, 'YO')
 // if the number is divisible by 5 log "buzz" instead of the number, 
 // and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
 function takeNum(n) {
-    for (i= 1; i< n; i++) {
-        if (i / 3 === 0) {
+    for (i= 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log('fizz');
         }
-        else if (i / 5 === 0) {
+        else if (i % 5 === 0) {
             console.log('buzz');
         }
-        else if (i / 3 === 0 && i / 5 === 0) {
+        else if (i % 3 === 0) {
             console.log('fizzbuzz');
         }
         else {
